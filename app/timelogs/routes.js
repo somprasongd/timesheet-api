a4dff6f5adc4f0fa69204c8ea27b443c8b77909e
Somprasong Damyos <somprasong.d@me.com>
2017-10-04 10:07:03 +0700

    use userId from login

--- app/timelogs/routes.js
+++ app/timelogs/routes.js
@@ -4,9 +4,8 @@ const auth = require('../../middlewares/auth')
 
 exports.setup = function(router) {
   router
-  .post('/checkin/:userId/', auth, (req, res) => {
-    console.log(req.auth)
-    const {userId, type = 1} = req.params
+  .post('/checkin/', auth, (req, res) => {
+    const {sub: userId, type = 1} = req.auth
     UserController.create(userId, type)
     .then(data => {
       res.status(201).json(data)
@@ -15,8 +14,8 @@ exports.setup = function(router) {
       res.status(500).json({status: 500, message: err.message || err})
     })
   })
-  .post('/checkout/:userId/', auth, (req, res) => {
-    const {userId, type = 2} = req.params
+  .post('/checkout', auth, (req, res) => {
+    const {sub: userId, type = 2} = req.auth
     UserController.create(userId, type)
     .then(data => {
       res.status(201).json(data)
